f5d33488963032aba6dce634e309f8ac5123cc6b
Jamie Wright <jamie@brilliantfantastic.com>
2016-03-03 07:21:02 -0500

    Complete startup and shutdown of the factory

--- lib/gotcha-bot/factory.rb
+++ lib/gotcha-bot/factory.rb
@@ -1,7 +1,43 @@
+require "eventmachine"
+
 module GotchaBot
+  class AlreadyStartedError < StandardError; end
+
   class Factory
     def self.startup!(configuration={})
+      raise AlreadyStartedError if running?
+
       GotchaBot.configure { |config| config.copy configuration }
+      instance
+    end
+
+    def self.shutdown!
+      EM.add_shutdown_hook { @instance = nil }
+      EM.stop
+      begin; end until !running?
+    end
+
+    def self.instance
+      @instance ||= new
+    end
+
+    def self.running?
+      EM.reactor_running?
+    end
+
+    private
+
+    def initialize
+      start!
+    end
+
+    def start!
+      factory_thread = Thread.new do
+        EM.run do
+          # TODO: Start all the teams in the database
+        end
+      end
+      factory_thread.abort_on_exception = true
     end
   end
 end
--- spec/gotcha-bot/factory_spec.rb
+++ spec/gotcha-bot/factory_spec.rb
@@ -1,9 +1,36 @@
 RSpec.describe GotchaBot::Factory do
   describe ".startup!" do
+    after { described_class.shutdown! }
+
     it "sets the configuration for the factory" do
       described_class.startup!(slack_key: "KEY", slack_secret: "SECRET")
       expect(GotchaBot.configuration.slack_key).to eq "KEY"
       expect(GotchaBot.configuration.slack_secret).to eq "SECRET"
+    end
+
+    it "does not allow multiple factories to be started" do
+      described_class.startup!
+      begin; end until described_class.running?
+      expect { described_class.startup! }.to \
+        raise_error GotchaBot::AlreadyStartedError
+    end
+
+    xit "starts the event loop to spawn existing bots"
+  end
+
+  describe ".shutdown!" do
+    before do
+      described_class.startup!
+      begin; end until described_class.running?
+      described_class.shutdown!
+    end
+
+    it "stops the event loop" do
+      expect(EM).to_not be_reactor_running
+    end
+
+    it "kills the instance" do
+      expect(described_class.instance_variable_get("@instance")).to be_nil
     end
   end
 end
