3451b0c7004c5f2bdd99257894c40e61f664a05e
Jamie Wright <jamie@brilliantfantastic.com>
2016-03-09 10:30:55 -0500

    Fix warning in spec

--- spec/gotcha_bot_spec.rb
+++ spec/gotcha_bot_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe GotchaBot do
   describe ".configure" do
     it "yields an instance of configuration" do
-      expect { |b| described_class.configure &b }.to \
+      expect { |b| described_class.configure(&b) }.to \
         yield_with_args(GotchaBot::Configuration)
     end
   end
